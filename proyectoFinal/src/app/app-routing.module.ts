e22f3e49c18440601caf381cfddc610768a5b277
Adrian <adriangarzonpozo@gmail.com>
2021-03-20 12:11:29 +0100

    Nav hecho con Route

--- proyectoFinal/src/app/app-routing.module.ts
+++ proyectoFinal/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'sobre-mi', component: AboutComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'crear-proyecto', component: CreateComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- proyectoFinal/src/app/app.module.ts
+++ proyectoFinal/src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    CreateComponent,
+    ProjectsComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
