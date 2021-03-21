e07669a3063f6f0e94698ca767eb7c51f271b08a
Adrian <adriangarzonpozo@gmail.com>
2021-03-21 11:49:54 +0100

    Guardar un proyecto, sin foto

--- proyectoFinal/src/app/components/create/create.component.ts
+++ proyectoFinal/src/app/components/create/create.component.ts
@@ -12,19 +12,34 @@ export class CreateComponent implements OnInit {
 
   title: string;
   project: Project;
+  status: string;
 
   constructor(
     private _projectService: ProjectService
-  ) { 
-    this.title= "Crear proyecto";
-    this.project=new Project('','','','',2019,'','');
+  ) {
+    this.title = "Crear proyecto";
+    this.project = new Project('', '', '', '', 2019, '', '');
+    this.status = "";
   }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any){
+  onSubmit(form: any) {
     console.log(this.project);
+    this._projectService.saveProject(this.project).subscribe(
+      (response: any) => {
+        if (response.project) {
+          this.status = "success";
+          form.reset();
+        } else {
+          this.status = "failed";
+        }
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 
 }
--- proyectoFinal/src/app/services/global.ts
+++ proyectoFinal/src/app/services/global.ts
@@ -1,3 +1,3 @@
 export var global={
-    url: 'http://localhost:3700/api/'
+    url: 'http://localhost:3700/api'
 };
